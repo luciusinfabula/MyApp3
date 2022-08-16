5dd812ade311f9e04779d1060a7715105d5688c5
Luciano Dal monte <lucdm@hotmail.it>
2022-08-16 11:20:10 +0200

    Aggiungere i file di progetto.

---
+++ MyApp3/MyApp3.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace MyApp3.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new MyApp3.App());
+            host.Run();
+        }
+    }
+}
---
+++ MyApp3/MyApp3.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace MyApp3.Skia.Gtk
+{
+}
